59530bc1790d47179ecb91d0156f97f13ffc1454
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-11-12 22:49:23 +0100

    working on ChainBomb

--- src/main/java/sk/tuke/kpi/oop/game/ChainBomb.java
+++ src/main/java/sk/tuke/kpi/oop/game/ChainBomb.java
@@ -4,16 +4,22 @@ import org.jetbrains.annotations.NotNull;
 import sk.tuke.kpi.gamelib.Actor;
 import sk.tuke.kpi.gamelib.Scene;
 
+import java.awt.geom.Ellipse2D;
+
 
 public class ChainBomb extends TimeBomb {
+
 	public ChainBomb(@NotNull float time) {
 		super(time);
 	}
 
 	public void removedFromScene(Scene scene) {
+		Ellipse2D round = new Ellipse2D.Float(this.getPosX() + 8,
+			this.getPosY() + 8, 50f, 50f);
 		for (Actor bomb : scene.getActors()) {
-			if (bomb.getClass() != ChainBomb.class) continue;
-
+			if (!(bomb instanceof ChainBomb)) continue;
+			if (round.contains(bomb.getPosX(), bomb.getPosY(), 16, 16))
+				((ChainBomb) bomb).activate();
 		}
 	}
 }
