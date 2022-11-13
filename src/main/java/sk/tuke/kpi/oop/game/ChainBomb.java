1fb3a4a9978018a49f814d31444840ae72c28d51
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-11-13 18:05:04 +0100

    bug fixing ChainBomb

--- src/main/java/sk/tuke/kpi/oop/game/ChainBomb.java
+++ src/main/java/sk/tuke/kpi/oop/game/ChainBomb.java
@@ -16,10 +16,10 @@ public class ChainBomb extends TimeBomb {
 	@Override
 	public void removedFromScene(Scene scene) {
 		super.removedFromScene(scene);
-		Ellipse2D round = new Ellipse2D.Float(this.getPosX() - 42,
-			this.getPosY() - 42, 120f, 120f);
+		Ellipse2D round = new Ellipse2D.Float(this.getPosX() - 48,
+			this.getPosY() - 48, 120f, 120f);
 		for (Actor bomb : scene.getActors()) {
-			if (bomb instanceof ChainBomb && round.intersects(bomb.getPosX() - 8, bomb.getPosY() - 8, bomb.getWidth() * 2, bomb.getHeight() * 2)) {
+			if (bomb instanceof ChainBomb && round.intersects(bomb.getPosX(), bomb.getPosY(), bomb.getWidth() * 2, bomb.getHeight() * 2)) {
 				//System.out.println(round.intersects(bomb.getPosX(), bomb.getPosY(), 16, 16));
 				((ChainBomb) bomb).activate();
 			}
