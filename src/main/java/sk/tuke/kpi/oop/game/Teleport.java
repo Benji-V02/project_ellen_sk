2c9071045ff6395efe68dc8e1e0cbdaac997a8d3
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-11-13 15:56:36 +0100

    made working Teleport

--- src/main/java/sk/tuke/kpi/oop/game/Teleport.java
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -1,19 +1,38 @@
 package sk.tuke.kpi.oop.game;
 
+import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.actions.Invoke;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.framework.Player;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
 import sk.tuke.kpi.gamelib.graphics.Animation;
+
+import java.awt.geom.Rectangle2D;
 
 public class Teleport extends AbstractActor {
 
 	//TODO: Make reaction with Player
 	private Teleport destination;
+	private boolean isPorted;
 
 	public Teleport() {
 		setAnimation(new Animation("sprites/lift.png"));
 		destination = null;
+		isPorted = false;
 	}
 
+	private void tpTest() {
+		Player player = (Player) super.getScene().getFirstActorByName("Player");
+		Rectangle2D playerPos = new Rectangle2D.Float(player.getPosX() + player.getWidth() / 2 - .5f, player.getPosY() + player.getHeight() / 2 - .5f,
+			1, 1);
+		//System.out.println(this.intersects(player));
+		Rectangle2D tp = new Rectangle2D.Float(this.getPosX(), this.getPosY(), this.getWidth(), this.getHeight());
+		//if(this.intersects(player))
+		if (tp.intersects(playerPos))
+			teleportPlayer(player);
+		else this.isPorted = false;
+	}
 
 	public Teleport getDestation() {
 		return destination;
@@ -23,14 +42,19 @@ public class Teleport extends AbstractActor {
 		this.destination = destinationTeleport;
 	}
 
-
-	public void teleportPlayer(Player player) {
-		if (destination == null || player == null) return;
-		if (player.getPosX() == this.getPosX() && player.getPosY() == this.getPosY()) {
-			player.setPosition(destination.getPosX(), destination.getPosY());
-
-		}
-
+	public void setPorted(boolean isPorted) {
+		this.isPorted = isPorted;
 	}
 
+	public void teleportPlayer(Player player) {
+		if (destination == null || player == null || isPorted) return;
+		player.setPosition(destination.getPosX(), destination.getPosY());
+		this.destination.setPorted(true);
+	}
+
+	@Override
+	public void addedToScene(@NotNull Scene scene) {
+		super.addedToScene(scene);
+		new Loop<>(new Invoke<Teleport>(this::tpTest)).scheduleFor(this);
+	}
 }
