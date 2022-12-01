d8e9d029787e4f2f4fa7e19ae471360621d1890d
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-12-02 00:25:24 +0100

    bug fixing, loosing my sanity

--- src/main/java/sk/tuke/kpi/oop/game/actions/Use.java
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Use.java
@@ -1,8 +1,6 @@
 package sk.tuke.kpi.oop.game.actions;
 
-import org.jetbrains.annotations.NotNull;
 import sk.tuke.kpi.gamelib.Actor;
-import sk.tuke.kpi.gamelib.Disposable;
 import sk.tuke.kpi.gamelib.framework.actions.AbstractAction;
 import sk.tuke.kpi.oop.game.items.Usable;
 
@@ -10,18 +8,12 @@ import sk.tuke.kpi.oop.game.items.Usable;
 public class Use<A extends Actor> extends AbstractAction<A> {
 
 	private final Usable<A> actor;
-	private A guy;
 
 
 	public Use(Usable<A> actor) {
 		this.actor = actor;
 	}
 
-	@Override
-	public @NotNull Disposable scheduleFor(@NotNull A actor) {
-		guy = actor;
-		return super.scheduleFor(actor);
-	}
 
 	@Override
 	public void execute(float deltaTime) {
