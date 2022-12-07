a24bc360ce09337faa3e2948bdf62f1678e85684
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-12-07 21:24:54 +0100

    bug fixing

---
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Shift.java
@@ -0,0 +1,16 @@
+package sk.tuke.kpi.oop.game.actions;
+
+import sk.tuke.kpi.gamelib.framework.actions.AbstractAction;
+import sk.tuke.kpi.oop.game.Keeper;
+
+public class Shift<K extends Keeper> extends AbstractAction<K> {
+	@Override
+	public void execute(float deltaTime) {
+		if (getActor() == null) return;
+		if (!isDone()) {
+			getActor().getBackpack().shift();
+			setDone(true);
+		}
+
+	}
+}
