0017d59a786d66a933fc1ae51f20f782747d5246
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-12-07 21:03:36 +0100

    bug fixing

--- src/main/java/sk/tuke/kpi/oop/game/actions/Drop.java
+++ src/main/java/sk/tuke/kpi/oop/game/actions/Drop.java
@@ -10,6 +10,15 @@ public class Drop<K extends Keeper> extends AbstractAction<K> {
 
 	@Override
 	public void execute(float deltaTime) {
+		try {
+			getActor().getScene().addActor(
+				getActor().getBackpack().peek(),
+				getActor().getPosX() + getActor().getWidth() / 2 - getActor().getBackpack().peek().getPosX() + getActor().getBackpack().peek().getWidth() / 2,
+				getActor().getPosY() + getActor().getHeight() / 2 - getActor().getBackpack().peek().getPosY() + getActor().getBackpack().peek().getHeight() / 2
+			);
+			getActor().getBackpack().remove(getActor().getBackpack().peek());
+		} catch (Exception ex) {
 
+		}
 	}
 }
