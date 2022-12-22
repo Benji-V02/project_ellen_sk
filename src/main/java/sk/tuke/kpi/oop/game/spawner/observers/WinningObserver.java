047c46e99385ba29465ec5dbe41bc308af946a32
Samuel Králik <samuel.kralik@student.tuke.sk>
2022-12-22 09:24:09 +0100

    Working on spawner and his observers

---
+++ src/main/java/sk/tuke/kpi/oop/game/spawner/observers/WinningObserver.java
@@ -0,0 +1,15 @@
+package sk.tuke.kpi.oop.game.spawner.observers;
+
+public class WinningObserver extends Observer {
+	private int enemiesRemaining;
+
+	public WinningObserver() {
+		this.enemiesRemaining = 10;
+	}
+
+	@Override
+	public void update() {
+		enemiesRemaining--;
+		if (enemiesRemaining == 0)
+	}
+}
